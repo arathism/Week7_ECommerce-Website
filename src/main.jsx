3358c56257b2814192c0aedb7f92b9ad482e5890
arathism <arathism74@gmail.com>
2026-07-19 15:10:01 +0530

    Fix React Router basename for GitHub Pages subpath

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <CartProvider>
           <App />
